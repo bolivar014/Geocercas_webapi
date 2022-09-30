a9cd5fc4ce5519a080e1fd635701d299b9e532e3
Camilo Valdes <bolivar014@gmail.com>
2022-09-30 14:13:25 -0500

    ajustando codigo

--- src/api/API/Program.cs
+++ src/api/API/Program.cs
@@ -1,6 +1,11 @@
+using ConfigSQLSERVER;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string connStringR = builder.Configuration.GetConnectionString("DefaultRead");
+builder.Services.AddDbContext<ApplicationDbContextRSQLSERVER>(options => options.UseSqlServer(connStringR));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ src/persistence/ConfigSQLSERVER/ApplicationDbContextRSQLSERVER.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigSQLSERVER
+{
+    public class ApplicationDbContextRSQLSERVER : DbContext
+    {
+        public ApplicationDbContextRSQLSERVER(DbContextOptions<ApplicationDbContextRSQLSERVER> options)
+        : base(options)
+        {
+
+        }
+
+        // Creando objeto models de PosicionesSatelitalesVehiculosPruebas
+        public DbSet<PosicionesSatelitalesVehiculosPruebas> PosicionesSatelitalesVehiculosPruebass { get; }
+
+    }
+}
